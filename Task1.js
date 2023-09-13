6d9dbc99eb542b42bdefae19faad2f15931e7089
FaiNasserAlkahtani <mariamkalenizi@gmail.com>
2023-09-13 20:04:23 +0300

    first commit

---
+++ Task1.js
@@ -0,0 +1,37 @@
+//Q1 +Q2
+const myArray = ["apple", "banana", "cherry"];
+//Q3
+console.log(myArray[0]);
+
+//Q4
+console.log(myArray[myArray.length - 1]);
+//Q5
+myArray.unshift("mango");
+console.log(myArray);
+
+//Q6
+myArray.pop();
+console.log(myArray);
+
+//Q7
+const numbers = [5, 10, 15, 20, 25];
+numbers.splice(1, 1);
+console.log(numbers);
+
+//Q8
+function getLength(numbers) {
+  let arraylength = numbers.length;
+  console.log(arraylength);
+}
+getLength(numbers);
+
+//Q9
+const temperatures = [72, 68, 74, 80, 76];
+
+function getMax(temperatures) {
+  if (temperatures[0] > temperatures[1]) {
+    console.log(temperatures[0]);
+  } else console.log[1];
+}
+
+getMax(temperatures);
